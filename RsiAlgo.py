f8eb395cb6afba2cbd0b31bf2018ebe19b9fa464
seanmurphy355 <51139158+seanmurphy355@users.noreply.github.com>
2020-11-17 14:09:14 -0800

    Add files via upload

--- RsiAlgo.py
+++ RsiAlgo.py
@@ -18,7 +18,10 @@ import statistics
 
 class BasicRSItrading(QCAlgorithm):
     '''Basic template algorithm simply initializes the date range and cash'''
-
+    highestTQQQPrice = 0
+    stopMarketTicket = None
+    stopMarketOrderFillTime = datetime.min
+    
     def Initialize(self):
           # window size
         self.window_size = 14
@@ -38,7 +41,6 @@ class BasicRSItrading(QCAlgorithm):
         self.dji.SetDataNormalizationMode(DataNormalizationMode.Raw) #DJI set
         self.rut= self.AddEquity("^RUT", Resolution.Daily)
         self.rut.SetDataNormalizationMode(DataNormalizationMode.Raw) #RUT set
-        self.Debug("Starting Params taken")
         self.NUM = 0
         self.HIGH = 0
         self.NUM_Med = 0
@@ -53,11 +55,7 @@ class BasicRSItrading(QCAlgorithm):
         if self.IsWarmingUp:
             return
         
-        #Not working at the moment
-        if self.Rebalance > 0:
-            if self.Rebalance >= 7:
-                self.Rebalance = 0
-            self.Rebalance += 1
+        if (self.Time - self.stopMarketOrderFillTime).days < 15:
             return
         
         'Set holdings and base data that is to be used'
@@ -68,125 +66,83 @@ class BasicRSItrading(QCAlgorithm):
         holdings_DJI = self.Portfolio['DJI'].Quantity
        
             
-        'Check to see if this is a good Trade based on the current RSI of the TQQQ'
+        'Check to see if this is a good Trade based on the current RSI of the TQQQ in this case we are over sold '
         if rsiValue < 30:
+            #Check Flags as well as starting marketTicket
             value = self.NUM
             Check = self.Check
-            
-            #Current daily price        
-            Current_TQQQ_Price = self.Securities['TQQQ'].Price
-            Current_Rut_Price = self.Securities['^RUT'].Price
-            Current_DJI_Price = self.Securities['DJI'].Price
-            Current_SPY_Price = self.Securities['SPY'].Price
-            
-            #set up the intial dist of the portfolio
+        
+            #set up the intial dist of the portfolio as well as starting marketTicket
             if value == 0:
                 self.SetHoldings("TQQQ",.70)
                 self.SetHoldings("DJI",.10)
                 self.SetHoldings("^RUT",.10)
                 self.SetHoldings("SPY",.10)
+                #Generate a stop Market ticket and a Stop market order that is to sell all Tqqq holdings if price drops below close
+                self.stopMarketTicket = self.StopMarketOrder("TQQQ", -holdings_TQQQ, 0.85 * self.highestTQQQPrice)
+                self.Debug("enter0")
         
-            #set up the sell and buy targets for the portfolio
-            if value == 0:
-                Tqqq_liq_Price = self.Securities['TQQQ'].Price*.40 + self.Securities['TQQQ'].Price
-         
-                #Stop loss vars
-                Tqqq_Limit_price = self.Securities['TQQQ'].Price *.92
-                Tqqq_Market_price = self.Securities['TQQQ'].Price *.86
-                Tqqq_First_Sell_Target = self.Securities['TQQQ'].Price *.25 + self.Securities['TQQQ'].Price 
-                Tqqq_Sec_Sell_Target = self.Securities['TQQQ'].Price *.50 + self.Securities['TQQQ'].Price
-                Starting_Hold = self.Portfolio['TQQQ'].Quantity
-                cash_out = 1
-    
-
-            
+            #Adaptivee Stop Loss Strat
+            if self.Securities["TQQQ"].Close > self.highestTQQQPrice:
+                self.highestTQQQPrice = self.Securities["TQQQ"].Close
+                updateFields = UpdateOrderFields()
+                updateFields.StopPrice = self.highestTQQQPrice * 0.9
+                self.stopMarketTicket.Update(updateFields)
+                self.Debug("TQQQ: " + str(self.highestTQQQPrice) + " Stop: " + str(updateFields.StopPrice))
                 
-            #Liquidate if we are up 50% on a traded
-            if Tqqq_liq_Price <= Current_TQQQ_Price:
-                self.Liquidate("DJI")
-                self.Liquidate("^RUT")
-                self.Liquidate("SPY")
-                self.Liquidate("TQQQ")
-            
-            
-            'Stop loss cases'
-            self.StopMarketOrder("TQQQ", -(holdings_TQQQ*.40), Tqqq_Limit_price)
-            self.StopMarketOrder("TQQQ",-(holdings_TQQQ), Tqqq_Market_price)
             #after we do our stopMarket order to sell all holdings we now need to liquidate everything
             if holdings_TQQQ == 0:
                 self.Liquidate("^DJI")
                 self.Liquidate("^RUT")
                 self.Liquidate("SPY")
-            
+                
             #Set flags to starting values
             self.NUM = 1
             self.HIGH = 0
             self.NUM_Med = 0
-            cash_out = 0
-            
-    
-    
-      
-
+        
+        
+        'Check to see if this is a good Trade based on the current RSI of the TQQQ In this case we are over bought'
         if rsiValue > 70:
-            
+            #Check Flags as well as starting marketTicket
             value = self.HIGH
             Check = self.Check
             
-            #Current daily price        
-            Current_TQQQ_Price = self.Securities['TQQQ'].Price
-            Current_Rut_Price = self.Securities['^RUT'].Price
-            Current_DJI_Price = self.Securities['DJI'].Price
-            Current_SPY_Price = self.Securities['SPY'].Price
-            
-            
             #set up the intial dist of the portfolio
+            #Generate a stop Market ticket and a Stop market order that is to sell all Tqqq holdings if price drops below close
             if value == 0:
-                self.SetHoldings("TQQQ",.40)
-                self.SetHoldings("^RUT",.20)
-                self.SetHoldings("DJI",.20)
-                self.SetHoldings("SPY",.20)
-                self.Debug("Starting Params taken")
+                self.SetHoldings("TQQQ",.25)
+                self.SetHoldings("^RUT",.25)
+                self.SetHoldings("DJI",.25)
+                self.SetHoldings("SPY",.25)
+                self.stopMarketTicket = self.StopMarketOrder("TQQQ", -holdings_TQQQ, 0.90 * self.highestTQQQPrice)
                 self.Debug("enter1")
-                
             
-            #set up the sell and buy targets for the portfolio
-            if value == 0:
-                Tqqq_liq_Price = self.Securities['TQQQ'].Price*.35 + self.Securities['TQQQ'].Price
-                #Stop loss vars
-                Tqqq_Limit_price = self.Securities['TQQQ'].Price *.94
-                Tqqq_Market_price = self.Securities['TQQQ'].Price *.87
-                Tqqq_First_Sell_Target = self.Securities['TQQQ'].Price *.25 + self.Securities['TQQQ'].Price 
-                Tqqq_Sec_Sell_Target = self.Securities['TQQQ'].Price *.50 + self.Securities['TQQQ'].Price
-                Starting_Hold = self.Portfolio['TQQQ'].Quantity
-                self.Debug("enter2")
-                cash_out = 1
-                
-                
-            #Liquidate if we are up 35% on a traded
-            if Tqqq_liq_Price <= Current_TQQQ_Price:
-                self.Liquidate("DJI")
-                self.Liquidate("^RUT")
-                self.Liquidate("SPY")
-                self.Liquidate("TQQQ")
+            #make sure that the stop ticket was set before updating the adaptive stop fields.
+            if value == 1:
+                if self.Securities["TQQQ"].Close > self.highestTQQQPrice:
+                    self.highestTQQQPrice = self.Securities["TQQQ"].Close
+                    updateFields = UpdateOrderFields()
+                    updateFields.StopPrice = self.highestTQQQPrice * 0.9
+                    self.stopMarketTicket.Update(updateFields)
+                    self.Debug("TQQQ: " + str(self.highestTQQQPrice) + " Stop: " + str(updateFields.StopPrice))
            
-           
-            self.StopMarketOrder("TQQQ", -(holdings_TQQQ*.40), Tqqq_Limit_price)
-            self.StopMarketOrder("TQQQ",-holdings_TQQQ, Tqqq_Market_price)
-            
+                
             #after we do our stopMarket order to sell all holdings we now need to liquidate everything
             if holdings_TQQQ == 0:
                 self.Liquidate("^DJI")
                 self.Liquidate("^RUT")
                 self.Liquidate("SPY")
+                
                 
             #Set flags to starting values
             self.NUM = 0
             self.NUM_Med = 0
             self.High = 1
-            cash_out = 0
-        
             
+            
+        
+        #limbo
         else:
             value = self.NUM_Med
             #set up the intial dist of the portfolio
@@ -196,12 +152,7 @@ class BasicRSItrading(QCAlgorithm):
             self.SetHoldings("DJI",.20)
             self.Debug("distrib")
             
-            if value == 0:
-                #Stop loss vars
-                Tqqq_Limit_price = self.Securities['TQQQ'].Price *.90
-                Tqqq_Market_price = self.Securities['TQQQ'].Price *.84
-          
-     
+    
             #after we do our stopMarket order to sell all holdings we now need to liquidate everything
             if holdings_TQQQ == 0:
                 self.Liquidate("^DJI")
@@ -212,3 +163,11 @@ class BasicRSItrading(QCAlgorithm):
             self.NUM = 0
             self.High = 0
             self.NUM_Med = 1
+            
+            
+    #order events
+    def OnOrderEvent(self, orderEvent):
+        if orderEvent.Status != OrderStatus.Filled:
+            return
+        if self.stopMarketTicket is not None and self.stopMarketTicket.OrderId == orderEvent.OrderId: 
+            self.stopMarketOrderFillTime = self.Time
