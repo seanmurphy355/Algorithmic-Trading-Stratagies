5a9fae03ce575f579dc474dc7099d6ea3611b661
seanmurphy355 <51139158+seanmurphy355@users.noreply.github.com>
2020-11-18 14:08:38 -0800

    Add files via upload

--- Zero_Risk_RSI.py
+++ Zero_Risk_RSI.py
@@ -1,3 +1,15 @@
+import numpy as np
+from clr import AddReference
+AddReference("System")
+AddReference("QuantConnect.Algorithm")
+AddReference("QuantConnect.Common")
+
+from System import *
+from QuantConnect import *
+from QuantConnect.Algorithm import *
+from QuantConnect.Brokerages import *
+from QuantConnect.Orders import *
+from QuantConnect.Data import *
 from QuantConnect.Data.UniverseSelection import *
 from QuantConnect.Algorithm.Framework.Execution import ExecutionModel
 import statistics
@@ -12,18 +24,18 @@ class BasicRSItrading(QCAlgorithm):
         self.window_size = 14
         self.SetWarmUp(self.window_size)
         
-        self.SetStartDate(2010,1, 1)  #Set Start Date
+        self.SetStartDate(2010,10, 25)  #Set Start Date
         self.SetEndDate(2020,10,25)    #Set End Date
         self.SetCash(100000)           #Set Strategy Cash
         self.Tqqq = self.AddEquity("TQQQ",Resolution.Daily)
         self.Tqqq.SetDataNormalizationMode(DataNormalizationMode.Raw) #set TQQQ
         self.spy = self.AddEquity("SPY", Resolution.Daily)
         self.spy.SetDataNormalizationMode(DataNormalizationMode.Raw) #set SPY
-        self.dji = self.AddEquity("DJI",Resolution.Daily)
-        self.dji.SetDataNormalizationMode(DataNormalizationMode.Raw) #DJI set
-        self.rut= self.AddEquity("^RUT", Resolution.Daily)
+        self.UDOW = self.AddEquity("UDOW",Resolution.Daily)
+        self.UDOW.SetDataNormalizationMode(DataNormalizationMode.Raw) #UDOW set
+        self.rut= self.AddEquity("URTY", Resolution.Daily)
         self.rut.SetDataNormalizationMode(DataNormalizationMode.Raw) #RUT set
-        self.rsi = self.RSI("TQQQ", 30,  MovingAverageType.Simple, Resolution.Daily)
+        self.rsi = self.RSI("TQQQ", 60,  MovingAverageType.Simple, Resolution.Daily)
 
   
     def OnData(self,data):
@@ -35,8 +47,8 @@ class BasicRSItrading(QCAlgorithm):
         rsiValue = self.rsi.Current.Value
         holdings_TQQQ = self.Portfolio['TQQQ'].Quantity
         holdings_SPY = self.Portfolio['SPY'].Quantity
-        holdings_RUT = self.Portfolio['^RUT'].Quantity
-        holdings_DJI = self.Portfolio['DJI'].Quantity
+        holdings_RUT = self.Portfolio['URTY'].Quantity
+        holdings_UDOW = self.Portfolio['UDOW'].Quantity
        
             
         'Check to see if this is a good Trade based on the current RSI of the TQQQ in this case we are over sold '
@@ -45,8 +57,8 @@ class BasicRSItrading(QCAlgorithm):
             #set up the intial dist of the portfolio as well as starting marketTicket
     
             self.SetHoldings("TQQQ",.70)
-            self.SetHoldings("DJI",.10)
-            self.SetHoldings("^RUT",.10)
+            self.SetHoldings("UDOW",.10)
+            self.SetHoldings("URTY",.10)
             self.SetHoldings("SPY",.10)
         
         'Check to see if this is a good Trade based on the current RSI of the TQQQ In this case we are over bought'
@@ -56,14 +68,14 @@ class BasicRSItrading(QCAlgorithm):
             #Generate a stop Market ticket and a Stop market order that is to sell all Tqqq holdings if price drops below close
          
             self.SetHoldings("TQQQ",.25)
-            self.SetHoldings("^RUT",.25)
-            self.SetHoldings("DJI",.25)
+            self.SetHoldings("URTY",.25)
+            self.SetHoldings("UDOW",.25)
             self.SetHoldings("SPY",.25)
            
         else:
 
             self.SetHoldings("TQQQ",.50)
             self.SetHoldings("SPY",.15)
-            self.SetHoldings("^RUT",.15)
-            self.SetHoldings("DJI",.20)
+            self.SetHoldings("URTY",.15)
+            self.SetHoldings("UDOW",.20)
             self.Debug("distrib")
